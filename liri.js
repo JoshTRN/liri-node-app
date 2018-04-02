9ad040b2f3774a1a80beb0a77e0338fc9ccc22ff
Joshua Wood <joshua@Joshuas-iMac.local>
2018-04-02 02:09:35 -0400

    everything perfectly functional just need api calls

--- liri.js
+++ liri.js
@@ -1,45 +1,138 @@
+//===============Require Calls=============//
 
+require('dotenv').config();
 var keys = require('./keys')
 var Spotify = require('node-spotify-api');
 var Twitter = require('twitter');
+var inquirer = require('inquirer');
 
 var spotify = new Spotify(keys.spotify);
 var client = new Twitter(keys.twitter);
 
-var typeExecution = process.argv[2];
+//===============Input Types=============//
 
+var typeExecution = process.argv[2];
 var tweets = 'my-tweets';
 var song = 'spotify-this-song';
 var movie = 'movie-this'
 
+//===============Song and Movie Varriables=============//
+var movieTitle = '';
+var songTitle = '';
+
+//===============Handler for Terminal Input=============//
 switch (typeExecution) {
 	case tweets:
-		console.log(tweets);
 		myTweets();
 		break;
 	case song:
-		console.log(song);
 		spotifySong();
 		break;
 	case movie:
-		console.log(movie);
-		movie();
+		getMovie();
 		break;
+	default:
+		initInquier();
 }
 
 
 
-//============Functions==========//
+//===============Functions=============//
 
 
-function myTweets(){
-
-}
-
-function spotifySong(){
+function myTweets() {
+	console.log(Twitter);
+	
 
 }
 
-function movie(){
+function spotifySong() {
+	// Refer to console input to extract the song title and store it into
+	// songTitle variable.
+	var len = process.argv.length
 
+	if (len > 3) {
+		for (var i = 3; i < len; i++){
+			if (i < len - 1 && songTitle !== ''){
+				songTitle = songTitle + ' ' + process.argv[i];
+			} else {
+				songTitle = process.argv[i];
+			}
+		}
+	} else {
+		console.log('Please add a song title.');
+	}
+	console.log(songTitle);
+}
+
+function getMovie() {
+	// Refer to console input to extract the song title and store it into
+	// movieTitle variable.
+	var len = process.argv.length
+	// Make sure a movie is added
+	if (len > 3) {
+		for (var i = 3; i < len; i++){
+			if (i < len && movieTitle !== ''){
+				movieTitle = movieTitle + ' ' + process.argv[i];
+			} else {
+				movieTitle = process.argv[i];
+			}
+		}
+	} else {
+		console.log('Please add a movie title.');
+	}	
+	// Log to make sure functioning.
+	console.log(movieTitle);
+
+}
+
+function initInquier() {
+	inquirer.prompt([
+		{
+			type: 'list',
+			message: 'Please select what you want to do?',
+			choices: ['my-tweets', 'spotify-this-song', 'movie-this'],
+			name: 'selection'
+		}
+		]).then(function(response){
+
+			switch (response.selection) {
+				case tweets:
+					myTweets();
+					break;
+				case song:
+					inquireSong();
+					break;
+				case movie:
+					inquireMovie();
+				break;
+
+			}
+		})
+}
+
+function inquireSong() {
+	inquirer.prompt([
+		{
+			type: 'input',
+			message: 'Tell me the song you want:',
+			name: 'songTitle'
+		}
+		]).then(function(response){
+			songTitle = response.songTitle;
+			spotifySong();
+		})
+}
+
+function inquireMovie() {
+	inquirer.prompt([
+		{
+			type: 'input',
+			message: 'Tell me the movie you want:',
+			name: 'movieTitle'
+		}
+		]).then(function(response){
+			songTitle = response.movieTitle;
+			getMovie();
+		})
 }
