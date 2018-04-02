6e7e271099586a99bad417c6d8375b9141bdd747
Joshua Wood <joshua@Joshuas-iMac.local>
2018-04-02 00:13:43 -0400

    created all the variables and created starter functions

--- liri.js
+++ liri.js
@@ -1,1 +1,45 @@
-require("dotenv").config();
+
+var keys = require('./keys')
+var Spotify = require('node-spotify-api');
+var Twitter = require('twitter');
+
+var spotify = new Spotify(keys.spotify);
+var client = new Twitter(keys.twitter);
+
+var typeExecution = process.argv[2];
+
+var tweets = 'my-tweets';
+var song = 'spotify-this-song';
+var movie = 'movie-this'
+
+switch (typeExecution) {
+	case tweets:
+		console.log(tweets);
+		myTweets();
+		break;
+	case song:
+		console.log(song);
+		spotifySong();
+		break;
+	case movie:
+		console.log(movie);
+		movie();
+		break;
+}
+
+
+
+//============Functions==========//
+
+
+function myTweets(){
+
+}
+
+function spotifySong(){
+
+}
+
+function movie(){
+
+}
